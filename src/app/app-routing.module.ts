40c965b92dd78656adce298475e4e6d5bcc3fc8d
ahmednaggarhossam <ahossamnaggar@gmail.com>
2023-03-30 20:24:53 +0200

    NewsApi Angular Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BusinessComponent } from './business/business.component';
+import { EntertainmentComponent } from './entertainment/entertainment.component';
+import { GeneralComponent } from './general/general.component';
+import { HealthComponent } from './health/health.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { ScienceComponent } from './science/science.component';
+import { SportsComponent } from './sports/sports.component';
+import { TechnologyComponent } from './technology/technology.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'general', pathMatch:'full'},
+  {path:'business', component:BusinessComponent , pathMatch:'full'},
+  {path:'entertainment', component:EntertainmentComponent , pathMatch:'full'},
+  {path:'general', component:GeneralComponent , pathMatch:'full'},
+  {path:'health', component:HealthComponent , pathMatch:'full'},
+  {path:'science', component:ScienceComponent , pathMatch:'full'},
+  {path:'sports', component:SportsComponent , pathMatch:'full'},
+  {path:'technology', component:TechnologyComponent , pathMatch:'full'},
+  {path:'**', component:NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GeneralComponent } from './general/general.component';
+import { BusinessComponent } from './business/business.component';
+import { EntertainmentComponent } from './entertainment/entertainment.component';
+import { HealthComponent } from './health/health.component';
+import { ScienceComponent } from './science/science.component';
+import { SportsComponent } from './sports/sports.component';
+import { TechnologyComponent } from './technology/technology.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GeneralComponent,
+    BusinessComponent,
+    EntertainmentComponent,
+    HealthComponent,
+    ScienceComponent,
+    SportsComponent,
+    TechnologyComponent,
+    NavbarComponent,
+    FooterComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
